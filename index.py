738889a11df194d53cc7a951041a04debaa9107d
João <Joaopauloresendecosta55@gmail.com>
2024-08-24 15:18:18 -0300

    AVALIACAO

--- index.py
+++ index.py
@@ -1,12 +1,16 @@
 from models.Main import restaurante
-
+import os
 restaurante_rua = restaurante('rua', 'Gourmet')
 restaurante_mexicano = restaurante('mexican Food', 'Nachos')
 restaurante_mexicano.switch_state()
 restaurante_rua.switch_state()
 
+
 def main():
-        restaurante.listar_restaurantes
+        os.system('cls')
+        restaurante.listar_restaurantes()
+        
+        
 
 
 if __name__ == '__main__':
