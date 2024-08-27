069d1c26d617eb71b680ae2c8dcaa678b1ea5634
João <Joaopauloresendecosta55@gmail.com>
2024-08-27 19:25:26 -0300

    Implements Project

--- PythonPrOject/app.py
+++ PythonPrOject/app.py
@@ -4,9 +4,11 @@ from menu.plate import Plate
 
 beverage = Beverage("suco melancia", 30.32, "700ml")
 
+plate = Plate("Camarão", 100.21, "Camarões")
+
 
 def main():
-    pass
+    print(plate)
 
 if __name__ == '__main__':
     main()
--- PythonPrOject/menu/beverage.py
+++ PythonPrOject/menu/beverage.py
@@ -4,3 +4,5 @@ class Beverage(ItemMenu):
           def __init__(self,name,price,size):
                   super.__init__(name,price)
                   self.size = size
+          def __str__(self):
+                  return self._name
