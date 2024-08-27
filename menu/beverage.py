59b615655e7bc9c1168fca2d6380ae9344b41e0e
João <Joaopauloresendecosta55@gmail.com>
2024-08-27 19:02:14 -0300

    extends

--- menu/beverage.py
+++ menu/beverage.py
@@ -2,4 +2,5 @@ from menu.Item_menu import ItemMenu
 
 class Beverage(ItemMenu):
           def __init__(self,name,price,size):
-                  pass
+                  super.__init__(name,price)
+                  self.size = size
